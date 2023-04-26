f7bf8ea1e84963602e5805ca97aba09d7c20321c
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-04-26 17:23:39 -0300

    Added index.jsx on routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle  from './styles/Global.js';
 import theme from './styles/Theme.js';
-import { Routes } from './routes';
+import { Routes } from './routes/index.jsx';
 import { AuthProvider } from './hooks/auth.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
